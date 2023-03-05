a5c2f7ec4dbc718518aa05cff612c373a23ce786
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2023-03-05 15:39:54 +0600

    done

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -9,107 +9,107 @@ const orderRouter = express.Router();
 
 // Get All
 orderRouter.get("/", async (req, res) => {
-  let data;
+    let data;
 
-  if (req?.query?.limit) {
-    data = await Order.find({}).sort("-date").limit(process.env.LIMIT);
-  } 
-  
-  else {
-    data = await (await Order.find({}).sort("-date")).reverse();
-  }
+    if (req?.query?.limit) {
+        data = await Order.find({}).sort("-date").limit(process.env.LIMIT);
+    } else if (req?.query?.userId) {
+        data = await Order.find({ userId: req?.query?.userId }).sort("-date");
+    } else {
+        data = await (await Order.find({}).sort("-date")).reverse();
+    }
 
-  try {
-    res.status(200);
-    res.send({
-      result: data,
-      message: "Success",
-    });
-  } catch (error) {
-    res.status(error.status || 500);
-    res.send({
-      error: error.message,
-    });
-  }
+    try {
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success",
+        });
+    } catch (error) {
+        res.status(error.status || 500);
+        res.send({
+            error: error.message,
+        });
+    }
 });
 
 // Create
 orderRouter.post("/", checkLoginHandler, async (req, res) => {
-  const newOrder = new Order(req.body);
+    const newOrder = new Order(req.body);
     // Submit To Database
     try {
-      const data = await newOrder.save();
-      res.status(200);
-      res.send({
-        result: data,
-        message: "Success",
-      });
+        const data = await newOrder.save();
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success",
+        });
     } catch (error) {
-      res.status(error.status || 500);
-      res.send({
-        error: error.message,
-      });
+        res.status(error.status || 500);
+        res.send({
+            error: error.message,
+        });
     }
 });
 
 // Update
 orderRouter.put("/:id", checkAdminHandler, async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
     // Submit To Database
     try {
-      const data = await Order.findByIdAndUpdate(id, req.body);
-      res.status(200);
-      res.send({
-        result: data,
-        message: "Success",
-      });
+        const data = await Order.findByIdAndUpdate(id, req.body);
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success",
+        });
     } catch (error) {
-      res.status(error.status || 500);
-      res.send({
-        error: error.message,
-      });
+        res.status(error.status || 500);
+        res.send({
+            error: error.message,
+        });
     }
 });
 
 // Delete
 orderRouter.delete("/:id", checkAdminHandler, async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
     // Submit To Database
     try {
-      const data = await Order.findByIdAndRemove(id);
-      res.status(200);
-      res.send({
-        result: data,
-        message: "Success",
-      });
+        const data = await Order.findByIdAndRemove(id);
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success",
+        });
     } catch (error) {
-      res.status(error.status || 500);
-      res.send({
-        error: error.message,
-      });
+        res.status(error.status || 500);
+        res.send({
+            error: error.message,
+        });
     }
 });
 
 // Single
 orderRouter.get("/:id", async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  // Submit To Database
-  try {
-    const data = await Order.findById(id).exec();
-    res.status(200);
-    res.send({
-      result: data,
-      message: "Success",
-    });
-  } catch (error) {
-    res.status(error.status || 500);
-    res.send({
-      error: error.message,
-    });
-  }
+    // Submit To Database
+    try {
+        const data = await Order.findById(id).exec();
+        res.status(200);
+        res.send({
+            result: data,
+            message: "Success",
+        });
+    } catch (error) {
+        res.status(error.status || 500);
+        res.send({
+            error: error.message,
+        });
+    }
 });
 
 // Export
